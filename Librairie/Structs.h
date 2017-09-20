aa77f355e82c2e4eafde8b41cdaf3f43be137e57
aragnac <nicolasbonemme@hotmail.com>
2017-09-20 11:35:44 +0200

    Update Structs.h

--- Librairie/Structs.h
+++ Librairie/Structs.h
@@ -4,11 +4,34 @@
 #ifndef SERVEUR_STRUCTS_H
 #define SERVEUR_STRUCTS_H
 
+#include <netdb.h>
+
 struct ip {
     unsigned char b1;
     unsigned char b2;
     unsigned char b3;
     unsigned char b4;
+};
+
+enum TypeRequete {
+    LOGIN_OFFICER  = 1,
+    LOGOUT_OFFICER   = 2,
+    CHECK_TICKET = 3
+};
+
+struct Requete {
+    enum TypeRequete Type;
+    char             Message[40];
+    int              Compteur;
+    int              Numero;
+    unsigned short   crc;
+    unsigned short   s0;
+};
+
+struct STBF {
+    int                DescPublic;
+    struct sockaddr_in psorPublic; /* r = remote */
+    struct Requete     UneRequeteR;
 };
 
 #endif //SERVEUR_STRUCTS_H
