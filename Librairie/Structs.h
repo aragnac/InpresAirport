8634a8218c85468fb0d7648b43deba4ff062316f
Floryan <floryansimar@gmail.com>
2017-09-20 11:34:20 +0200

    Création fichier structures

---
+++ Librairie/Structs.h
@@ -0,0 +1,14 @@
+//
+// Created by floryan on 20/09/17.
+//
+#ifndef SERVEUR_STRUCTS_H
+#define SERVEUR_STRUCTS_H
+
+struct ip {
+    unsigned char b1;
+    unsigned char b2;
+    unsigned char b3;
+    unsigned char b4;
+};
+
+#endif //SERVEUR_STRUCTS_H
